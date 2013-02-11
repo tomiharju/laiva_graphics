7f08c1545e34fd200b1b8b0fb37e7327ae20f4e5
Tomppa@Leppävaara <Tomibook@Tomibook-PC.wlan.metropolia.fi>
2013-02-11 18:00:54 +0200

    Väänsin jotain unitytunnilla

---
+++ Battleships/src/Controllers/TurnManager.java
@@ -0,0 +1,20 @@
+package Controllers;
+
+public class TurnManager {
+	
+	private final char my_turn = 'm';
+	private final char opponent_turn = 'o';
+	
+	
+	public TurnManager(){
+		
+	}
+	
+	
+	
+	public void PlayTurn(){
+		
+		
+	}
+
+}
